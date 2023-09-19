2fd430f2c7cac616314bfc870a5342d0e5bc3d3d
ChkKris <kristina141098@gmail.com>
2023-09-19 22:13:57 +0300

    Update

--- js/script.js
+++ js/script.js
@@ -8,10 +8,47 @@ for (let anchor of anchors) {
 			behavior: "smooth",
 			block: "start"
 		})
+
+		if (iconMenu.classList.contains('_active')) {
+			document.body.classList.remove('_lock');
+			iconMenu.classList.remove('_active');
+			menuBody.classList.remove('_active');
+		}
 	})
+	
 }
 
-document.querySelector('.burger').addEventListener('click', function () {
-	this.classList.toggle('active');
-	document.querySelector('.nav').classList.toggle('open');
-})
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+
+if (iconMenu) {
+	iconMenu.addEventListener('click', function (e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	});
+}
+
+
+
+const swiper = new Swiper('.info-icons__tablet', {
+	direction: 'horizontal',
+	loop: false,
+	loopedlides: 0,
+
+	freeMode: true,
+
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+	slidesPerView: 1,
+
+	spaceBetween: 24,
+
+	slidesPerGroup: 1,
+
+	speed: 800,
+});
+
